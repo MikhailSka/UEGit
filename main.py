1a9e3114bd62156262b741664fce8f9a46ddd780
Mikhail <mike46kan@gmail.com>
2023-12-28 16:44:51 +0100

    Bitcoin route !!!!!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from generate_random_phrase import generate_random_phrase
+from get_btc_to_usd_rate import get_btc_to_usd_rate
 import random
 
 app = Flask(__name__)
@@ -23,6 +24,15 @@ def get_random_phrase():
     num_words = 3  
     random_phrase = generate_random_phrase(num_words)
     return jsonify({'random_phrase': random_phrase})
+
+@app.route('/btc-to-usd', methods=['GET'])
+def get_btc_to_usd():
+    btc_to_usd_rate = get_btc_to_usd_rate()
+
+    if btc_to_usd_rate is not None:
+        return jsonify({'btc_to_usd': btc_to_usd_rate})
+    else:
+        return jsonify({'error': 'Failed to fetch BTC to USD exchange rate'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
